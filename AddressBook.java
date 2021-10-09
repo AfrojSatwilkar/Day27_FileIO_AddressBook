263e9c46b3f05be961ba49f36184183599f13381
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-10-09 12:02:30 +0530

    Welcome to address book program

---
+++ AddressBook.java
@@ -0,0 +1,8 @@
+package com.company1.Day9AddressBook;
+
+public class AddressBook {
+
+    public static void main(String[] args) {
+        System.out.print("Welcome to Address Book Program");
+    }
+}
